c2f476819806b1e99a02b4e94fc4b7bddc8092aa
Dip-Ka <143940154+Dip-Ka@users.noreply.github.com>
2023-09-17 10:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+// Initialize an empty string to store the FizzBuzz output
+let output = "";
+
+// Loop through numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+  // Check for multiples of both three and five first
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += "FizzBuzz";
+  }
+  // Check for multiples of three
+  else if (i % 3 === 0) {
+    output += "Fizz";
+  }
+  // Check for multiples of five
+  else if (i % 5 === 0) {
+    output += "Buzz";
+  }
+  // For all other numbers
+  else {
+    output += i.toString();
+  }
+
+  // Add a newline character after each number/string (except the last one)
+  if (i < 100) {
+    output += "\n";
+  }
+}
+
+// Display the FizzBuzz output using the alert function
+alert(output);
